f688deedf12d8a4d6b8ae3103199b559b47c2788
Jasper Eames Palmer <jpalmer4@uw.edu>
2020-05-08 16:07:24 -0700

    Add Support for Other Search Engines

--- Prompt_Search_Extension/background.js
+++ Prompt_Search_Extension/background.js
@@ -1,6 +1,14 @@
-// Updates the current tab's URL with google query of user's search field input
+// Updates the current tab's URL with search engine query of user's search field input (defaults to Google)
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if(message.popupOpen) {
-        chrome.tabs.update(null, {url: "https://www.google.com/search?q=" + message.popupOpen});
+        if (message.popupOpen.startsWith("url:")) {
+            chrome.tabs.update(null, {url: "" + message.popupOpen.substring(4)});
+        } else if (message.popupOpen.startsWith("ddg:")) {
+            chrome.tabs.update(null, {url: "https://duckduckgo.com/?q=" + message.popupOpen.substring(4)});
+        } else if (message.popupOpen.startsWith("spg:")){
+            chrome.tabs.update(null, {url: "https://www.startpage.com/do/dsearch?query=" + message.popupOpen.substring(4)});
+        } else {
+            chrome.tabs.update(null, {url: "https://www.google.com/search?q=" + message.popupOpen});
+        }
     }
 });
