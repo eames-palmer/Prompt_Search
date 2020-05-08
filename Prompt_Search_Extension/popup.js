2c9d6177899e6c2544b37fa9d41c4111e12a626d
Jasper Eames Palmer <jpalmer4@uw.edu>
2020-05-08 13:13:22 -0700

    Add Styling and Icons

--- Prompt_Search_Extension/popup.js
+++ Prompt_Search_Extension/popup.js
@@ -1,6 +1,12 @@
 // Stores a reference to the search field
 var search_input = document.getElementById('search_input');
 
+// Stores reference to icon image
+var icon = document.getElementById('icon');
+
+// Moves Cursor to Search Field
+search_input.focus();
+
 // Sends a message to background.js with input value on search input submission
 search_input.addEventListener('keypress', function() {
     if (event.key === "Enter") {
