9414e0ed8767b18447d991e46f71edcc2205d76f
Jasper Eames Palmer <jpalmer4@uw.edu>
2020-05-07 22:58:42 -0700

    Add Popup Window

--- Prompt_Search_Extension/background.js
+++ Prompt_Search_Extension/background.js
@@ -1,5 +1,7 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.browserAction.setBadgeText({text: 'ON'});
+    chrome.browserAction.setBadgeBackgroundColor({color: '#32ABE2'});
     chrome.tabs.executeScript({
         code: 'var prompt = prompt("Search:"); if (prompt) { window.location.href = ("https://www.google.com/search?q=" + prompt); }'
     });
