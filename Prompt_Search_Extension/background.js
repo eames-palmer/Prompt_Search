c3efd97a4d7cc27b76332de8e402c0e1b5002047
Jasper Eames Palmer <jpalmer4@uw.edu>
2020-06-16 14:39:59 -0700

    Add terminal style navigation

--- Prompt_Search_Extension/background.js
+++ Prompt_Search_Extension/background.js
@@ -1,9 +1,10 @@
 /*
- * Sets default search engine to google.
+ * Sets default search engine to google and initializes the query history.
  */
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({
-        defaultSearchEngine: 'google'
+        defaultSearchEngine: 'google',
+        queryHistory: []
     }, null);
 });
 
@@ -14,44 +15,49 @@ chrome.runtime.onInstalled.addListener(function() {
  * message: a message containing the search input value.
  */
 function handleQuery(message) {
-    chrome.storage.sync.get(['defaultSearchEngine'], function(result) {
-        /* Stores the default search engine */
+    chrome.storage.sync.get(['defaultSearchEngine'], result => {
+        /*
+         * Stores the default search engine.
+         */
         var engine = result.defaultSearchEngine;
-        if(message.popupOpen) {
-            if (message.popupOpen.startsWith("url:")) {
-                    chrome.tabs.update(null, {url: "https://"
-                        + message.popupOpen.substring(4).trim()});
-            } else {
-                switch (engine) {
-                    case 'google':
-                        chrome.tabs.update(null, {
-                            url: "https://www.google.com/search?q="
-                                + message.popupOpen
-                            }
-                        );
-                        break;
-                    case 'duckduckgo':
-                        chrome.tabs.update(null, {
-                            url: "https://duckduckgo.com/?q="
-                                + message.popupOpen
-                            }
-                        );
-                        break;
-                    case 'startpage' :
-                        chrome.tabs.update(null, {
-                            url: "https://www.startpage.com/do/dsearch?query="
-                                + message.popupOpen
-                            }
-                        );
-                        break;
-                    default:
-                        alert("Error with search engine preference");
-                        break;
-                }
+        if(!message.query) {
+            throw 'No Input';
+        }
+        if (message.query.startsWith('url:')) {
+                chrome.tabs.update(null, {url: 'https://'
+                    + message.query.substring(4).trim()});
+        } else {
+            switch (engine) {
+                case 'google':
+                    chrome.tabs.update(null, {
+                        url: 'https://www.google.com/search?q='
+                            + message.query
+                        }
+                    );
+                    break;
+                case 'duckduckgo':
+                    chrome.tabs.update(null, {
+                        url: 'https://duckduckgo.com/?q='
+                            + message.query
+                        }
+                    );
+                    break;
+                case 'startpage':
+                    chrome.tabs.update(null, {
+                        url: 'https://www.startpage.com/do/dsearch?query='
+                            + message.query
+                        }
+                    );
+                    break;
+                default:
+                    alert('Error with search engine preference');
+                    break;
             }
         }
     });
 }
 
-/* On message send the query to be handled */
+/*
+ * On message send the message containing a query to be handled.
+ */
 chrome.runtime.onMessage.addListener(handleQuery);
--- Prompt_Search_Extension/popup.js
+++ Prompt_Search_Extension/popup.js
@@ -1,14 +1,86 @@
-/* Stores a reference to the search field */
-var searchInput = document.getElementById('search_input');
+/*
+ * Stores a reference to the search field.
+ */
+const searchInput = document.getElementById('search_input');
 
-/* Moves Cursor to Search Field */
+/*
+ * Moves cursor to search field.
+ */
 searchInput.focus();
 
 /*
- * Sends a message to background.js with input value on search input submission
+ * Stores past enteries (the history) of the search input field.
  */
-searchInput.addEventListener('keypress', function() {
-    if (event.key === "Enter") {
-        chrome.runtime.sendMessage({popupOpen: searchInput.value});
+var queries;
+
+/*
+ * Stores the user's current location within the history.
+ */
+var queriesLocation;
+
+/*
+ * Initializes queries from storage.
+ */
+chrome.storage.sync.get(['queryHistory'], result => {
+    queries = result.queryHistory;
+    queriesLocation = queries.length;
+});
+
+/*
+ * Sends a message to background.js with input value on search input submission.
+ */
+searchInput.addEventListener('keydown', () => {
+    if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+        event.preventDefault();
     }
+    /* Defines the max capacity of the stored queries */
+    let maxQueries = 200;
+    if (queries.length === maxQueries) {
+        queries = queries.splice(0, 1);
+        queriesLocation--;
+    }
+    switch (event.key) {
+        case 'Enter':
+            if (searchInput.value !== '') {
+                queries[queriesLocation] = searchInput.value;
+                queriesLocation++;
+                chrome.storage.sync.set({
+                    queryHistory: queries
+                }, function() {
+                    chrome.runtime.sendMessage({query: searchInput.value});
+                });
+            }
+            break;
+        case 'ArrowUp':
+            if (queriesLocation > 0) {
+                queriesLocation--;
+                searchInput.value = queries[queriesLocation];
+            }
+            break;
+        case 'ArrowDown':
+            if (queries.length > queriesLocation) {
+                if (queries.length - 1 === queriesLocation) {
+                    queriesLocation++;
+                    searchInput.value = '';
+                } else {
+                    queriesLocation++;
+                    searchInput.value = queries[queriesLocation];
+                }
+            }
+            break;
+        default:
+            break;
+    }
+});
+
+/*
+ * Update query history with each new keypress.
+ */
+searchInput.addEventListener('keyup', () => {
+    if (queriesLocation >= 0 && queriesLocation < queries.length) {
+        queries[queriesLocation] = searchInput.value;
+    }
+    chrome.storage.sync.set({
+        queryHistory: queries
+    }, null);
 });
